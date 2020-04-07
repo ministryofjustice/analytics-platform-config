a8f17a3c647f13e7dffc9b2e715c8904d00e2471
Dhiraj Narwani <dhiraj.narwani@digital.justice.gov.uk>
2020-04-07 12:54:17 +0100

    add files

--- remove-gpg-keys.sh
+++ remove-gpg-keys.sh
@@ -6,6 +6,7 @@ KEY_FILE=$PWD/keys.txt
 
 while IFS= read -r key; do
     
+    rm $key
     # Remove git history for this specific file path to prevent users accessing old keys
     git filter-branch -f --index-filter "git rm --cached --ignore-unmatch .git-crypt/keys/default/0/$key" HEAD 
 
