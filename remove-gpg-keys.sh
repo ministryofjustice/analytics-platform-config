03d5fc0d64e7dc32adafdbec31ccde3baaa8049a
Dhiraj Narwani <dhiraj.narwani@digital.justice.gov.uk>
2020-04-07 13:03:48 +0100

    modify script

--- remove-gpg-keys.sh
+++ remove-gpg-keys.sh
@@ -2,29 +2,20 @@
 
 set -ex
 
-GPG_KEY=".git-crypt/keys/default/0/$key"
 KEY_FILE=$PWD/keys.txt
 
 while IFS= read -r key; do
     
+    GPG_KEY=".git-crypt/keys/default/0/$key"
+
     # Delete the .gpg key 
     if [[ -f $GPG_KEY ]]; then
         rm $GPG_KEY
+        git add .
+        git commit -m "Remove $key"
     fi
-
-done <$KEY_FILE 
-
-git add .
-git commit -m "Remove gpg keys"
-
-KEY_FILE=$PWD/keys.txt
-while IFS= read -r key; do
-
+    
     # Remove git history for this specific file path to prevent users accessing their old key
     git filter-branch -f --index-filter "git rm --cached --ignore-unmatch $GPG_KEY" HEAD 
 
 done <$KEY_FILE 
-
-    
-
-
