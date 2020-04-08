78dd8e85647a48c66e35488f41d02e0f8b6a17db
Dhiraj Narwani <dhiraj.narwani@digital.justice.gov.uk>
2020-04-08 16:56:23 +0100

    change history script to delete commits in branches

--- remove-gpg-keys.sh
+++ remove-gpg-keys.sh
@@ -7,6 +7,7 @@ KEY_FILE=$PWD/keys.txt
 while IFS= read -r key; do
     
     # Remove git history for this specific file path to prevent users accessing old keys
-    git filter-branch -f --index-filter "git rm --cached --ignore-unmatch .git-crypt/keys/default/0/$key" HEAD 
+    git filter-branch --force --index-filter "git rm --cached --ignore-unmatch .git-crypt/keys/default/0/$key" \
+    --prune-empty --tag-name-filter cat -- --all
 
 done <$KEY_FILE 
