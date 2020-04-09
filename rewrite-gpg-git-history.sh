6a01d0d4ea4f126bce4be876de30355ebc1ab638
Dhiraj Narwani <dhiraj.narwani@digital.justice.gov.uk>
2020-04-09 13:59:39 +0100

    remove extra pruning - not needed

--- rewrite-gpg-git-history.sh
+++ rewrite-gpg-git-history.sh
@@ -10,13 +10,4 @@ while IFS= read -r key; do
     git filter-branch --force --index-filter "git rm --cached --ignore-unmatch .git-crypt/keys/default/0/$key" \
     --prune-empty --tag-name-filter cat -- --all
 
-    git for-each-ref --format='delete %(refname)' refs/original | git update-ref --stdin
-
-    git reflog expire --expire=now --all
-
-    git gc --prune=now
-
-    git push origin --force --all
-    git push origin --force --tags
-
 done <$KEY_FILE 
